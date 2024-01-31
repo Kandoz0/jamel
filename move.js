6ffa65b62f8c2a5cbb9fd59fa8d8752b45dff67c
Kandoz0 <158366645+Kandoz0@users.noreply.github.com>
2024-01-31 23:39:09 +0100

    Add files via upload

---
+++ move.js
@@ -0,0 +1,11 @@
+
+
+ 
+ function content(x) {
+    x.style.position = "relative";
+    x.style.top =Math.floor(Math.random() * 200) +"px";
+    x.style.right=Math.floor(Math.random() * 200) +"px";
+    x.style.left=Math.floor(Math.random() * 200) +"px"
+ }
+
+ 
